9982e30a5edd646a3c71a5bf6052b75596c6711b
kosmet609 <140830823+kosmet609@users.noreply.github.com>
2023-08-03 14:39:29 +0200

    Add files via upload

---
+++ Assinment 1.js
@@ -0,0 +1,103 @@
+
+/*
+//Exercise 1
+let user = {
+    name: "John",
+    years: 30
+};
+
+var name =user.name;
+var age = user.years ;
+var isAdmin ;
+if ( user.isAdmin){
+    isAdmin = user.isAdmin;
+}else {
+    isAdmin = false;
+}
+console.log(name , age , isAdmin);
+*/
+
+
+// Exercise 2
+var Earth;
+var CurrentUser;
+
+// Exercise 3
+/*
+let phrase = "Hello"
+
+if (true) {
+    let user = "John";
+  function sayHi() {
+    alert(`${phrase}, ${user}`)
+  }
+}
+sayHi();   // for this situation the result will be an error because the js code isnt attached to html code but if we attach it to one the result will be alert 'Hello Jhon'
+*/
+
+// Exercise 4 
+/*
+var user = {};
+user.name = 'john';
+user.surName = 'smith';
+user.name = 'peter';
+delete user.name;
+console.log(user);
+*/
+
+// Exercise 5
+/*
+const user = {
+    name: "John"
+  }
+  
+  // does it work?
+  user.name = "Pete"
+ 
+  // it works!! we can change const object proberity but we cant change object value ex make it array 
+ */
+
+/*
+// Exercise 6 
+let salaries = { 
+    Fred: 100,
+    Ted: 160,
+    Ghaith: 130
+} ;
+  let sum = 0
+  if ( salaries.Fred && salaries.Ghaith && salaries.Ted) {
+    sum = salaries.Fred + salaries.Ghaith + salaries.Ted;
+  } else {
+    sum = 0 ;
+    
+  }
+  console.log(sum);
+*/
+
+/*
+// terniray operator 
+if (a + b < 4) {
+    result = 'Below';
+  } else {
+    result = 'Over';
+  }
+
+(a+b <4) ? result = 'Below' : result = 'Over';
+*/
+
+/*
+// terniray operator 2 
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+
+( login == 'Employee' ) ? message = 'Hello' : (login == 'Director') ? message = 'Greetings' : (login == '') ? message = 'No login' : message = '' ;
+*/ 
